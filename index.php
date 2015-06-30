335f9812f95297768c7a33c23c27f31025fd2731
scorp7mix <scorp7mix@gmail.com>
2015-06-30 22:55:25 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,26 @@
-<?phprequire_once 'autoload.php';use controllers\Application;session_start();set_exception_handler(    function ($e) {        echo 'exception: ';        echo $e->getMessage();    });$get = explode('/', $_GET['q']);$params = [];foreach ($get as $v) {    if ($v != '') {        $params[] = $v;    }}$app = new Application();$app->request('execute', $params);
+<?php
+
+use controllers\Application;
+
+session_start();
+
+require_once 'autoload.php';
+
+set_exception_handler(
+    function ($e) {
+        echo 'exception: ';
+        echo $e->getMessage();
+    }
+);
+
+$get = explode('/', $_GET['q']);
+$params = [];
+
+foreach ($get as $v) {
+    if ($v != '') {
+        $params[] = $v;
+    }
+}
+
+$app = new Application();
+$app->request('execute', $params);
